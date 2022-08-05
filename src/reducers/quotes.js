95b1187c068492a0c678d43d0074f0d5a5643381
Matthew <matthew_mcaleer@hotmail.com>
2022-08-05 18:13:29 +1000

    commit tree 4ecf9569f003badbe096b36cfa16a96f52871310

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,16 @@
-export default (state = [], action) => {
-  return state;
+function quotesReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+
+    case "REMOVE_QUOTE":
+      idx = state.findIndex(quote => quote.id  === action.id);
+      return [...state.slice(0, idx), ...state.slice(idx + 1)];
+
+    default:
+      return state;
+  }
 }
+
+export default quotesReducer;
